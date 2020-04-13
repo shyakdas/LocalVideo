838938d88c629e858b4714aebb6077a6a9257e3e
shayak.das007@gmail.com <shayak.das007@gmail.com>
2020-04-13 23:33:23 +0530

    [Implementation]OnSucess of bookmark data saved into database

--- app/src/main/java/com/application/localvideo/fragments/localvideo/LocalVideoFragment.kt
+++ app/src/main/java/com/application/localvideo/fragments/localvideo/LocalVideoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -82,6 +83,7 @@ open class LocalVideoFragment : BaseFragment(), VideoClickListener {
     }
 
     override fun onItemClick(videoModel: VideoModel) {
+        Toast.makeText(context, getString(R.string.video_saved), Toast.LENGTH_SHORT).show()
         videoViewModel.saveBookMark(videoModel)
     }
 }
